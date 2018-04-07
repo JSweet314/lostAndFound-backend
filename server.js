b74e3af146772deb01249f0e20d935923d7194b5
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-04-06 20:43:40 -0600

    Format new user error

--- server.js
+++ server.js
@@ -17,18 +17,42 @@ app.post('/api/v1/users/new', (request, response) => {
   for (let requiredParameter of ['username', 'email', 'password']) {
     if (!user[requiredParameter]) {
       return response.status(422)
-        .send({ error: `Expected format: {username: <String>, email: <String>, password: <String>}. You are missing a ${requiredParameter} property.` });
+        .send({ error: 
+          `Expected format: 
+            {username: <String>, email: <String>, password: <String>}. 
+          You are missing a ${requiredParameter} property.` 
+        });
     }
   }
 
   database('users').insert(user, 'id')
-    .then(user => { 
-      console.log(user);
-      response.status(201).json({id: user[0]}) 
+    .then(user => {
+      response.status(201).json({ id: user[0] })
     })
-    .catch(error => { 
+    .catch(error => {
       response.status(500).json(new Error(`insert user failed: ${error}`))
     });
+});
+
+app.post('/api/v1/users/signIn', (request, response) => {
+  const signInParams = request.body;
+  for (let requiredParameter of ['email', 'password']) {
+    if (!signInParams[requiredParameter]) {
+      return response.status(422)
+        .send({error: `Expected ${requiredParameter} parameter missing.`});
+    }
+  }
+
+  database('users')
+    .where('email', signInParams.email)
+    .andWhere('password', signInParams.password)
+    .then(user => {
+      console.log(user);
+      response.status(201).json({username: user[0].username, id: user[0].id});
+    })
+    .catch(error => {
+      response.status(500).json(new Error(`couldn't find user: ${error}`))
+    })
 });
 
 app.listen(3000, () => {
