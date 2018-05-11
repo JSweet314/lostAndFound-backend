941e09d5a6a955a8c2a941ca2d07f80998135b5a
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-05-11 13:30:18 -0600

    Add cors config to server

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/api/v1/users', (request, response) => {
   database('users').select()
